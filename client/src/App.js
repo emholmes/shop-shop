b17032a688c21bb64649d6f990f389364e113524
Erin Holmes <e.holmes09@yahoo.com>
2021-10-09 22:20:28 -0500

    install redux and react-redux, create global store and add to App.js

--- client/src/App.js
+++ client/src/App.js
@@ -14,7 +14,9 @@ import NoMatch from './pages/NoMatch';
 import Login from './pages/Login';
 import Signup from './pages/Signup';
 import Nav from './components/Nav';
-import { StoreProvider } from './utils/GlobalState';
+// import { StoreProvider } from './utils/GlobalState';
+import store from './utils/store';
+import { Provider } from 'react-redux';
 import OrderHistory from './pages/OrderHistory';
 import Success from './pages/Success';
 
@@ -42,7 +44,7 @@ function App() {
     <ApolloProvider client={client}>
       <Router>
         <div>
-          <StoreProvider>
+          <Provider store={store}>
             <Nav />
             <Switch>
               <Route exact path="/" component={Home} />
@@ -53,7 +55,7 @@ function App() {
               <Route exact path="/success" component={Success} />
               <Route component={NoMatch} />
             </Switch>
-          </StoreProvider>
+          </Provider>
         </div>
       </Router>
     </ApolloProvider>
